3b88a393b185f4f0efa06863ece8d58b2f433cf2
Wenze0614 <wenze0614@gmail.com>
2022-01-13 00:47:12 +0800

    add routes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/profile' />
+        <Route path='/projects' />
+      </Routes>
     </div>
   );
 }

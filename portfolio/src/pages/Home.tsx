eac2af6c943fa905dcbd0204a4bbd33ffba03898
Wenze0614 <wenze0614@gmail.com>
2022-01-13 01:59:40 +0800

    add content to home page

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import './Home.css'
 import Piano from "../assets/piano.mp4"
 import { useState } from 'react'
+
 export default function Home() {
-    const [play,setPlay] = useState(true)
+    const [play, setPlay] = useState(true)
     return (
         <div className='home'>
             <div className='video-container'>
@@ -10,7 +11,11 @@ export default function Home() {
                     <source id="mp4" src={Piano} type="video/mp4" />
                 </video>
             </div>
-            <button onClick={()=>setPlay(false)}>Play</button>
+            <div className='home-content'>
+                <button onClick={() => setPlay(false)}>Play</button>
+                <h2>About Me</h2>
+                <h2>Projects</h2>
+            </div>
         </div>
     )
 }

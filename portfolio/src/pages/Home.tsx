29e84c0dea8b945d0d31a6d90726a6513474fa9b
Wenze0614 <wenze0614@gmail.com>
2022-01-13 01:52:18 +0800

    add sound play button

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
 import './Home.css'
 import Piano from "../assets/piano.mp4"
+import { useState } from 'react'
 export default function Home() {
+    const [play,setPlay] = useState(true)
     return (
         <div className='home'>
             <div className='video-container'>
-                <video autoPlay loop muted>
+                <video autoPlay loop muted={play}>
                     <source id="mp4" src={Piano} type="video/mp4" />
                 </video>
             </div>
+            <button onClick={()=>setPlay(false)}>Play</button>
         </div>
     )
 }

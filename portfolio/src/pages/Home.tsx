7712ba5370d48981bd63afb4f13e1a4d48ea73c5
Wenze0614 <wenze0614@gmail.com>
2022-01-13 16:13:14 +0800

    add some media styling

--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import './Home.css'
 import Piano from "../assets/piano.mp4"
+import profile from '../assets/profile.JPG'
 import { useState, useRef, useEffect } from 'react'
 import Stack from '@mui/material/Stack';
 import Slider from '@mui/material/Slider';
 import VolumeDown from '@mui/icons-material/VolumeDown';
 import VolumeUp from '@mui/icons-material/VolumeUp';
-import{NavLink, Outlet, useNavigate} from 'react-router-dom'
+import Avatar from '@mui/material/Avatar';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 export default function Home() {
     const navigate = useNavigate()
     const [play, setPlay] = useState(false)
@@ -41,16 +43,18 @@ export default function Home() {
             </div>
             <div className={`home-content ${active ? "active" : ""}`}>
                 <h2 onClick={handlePlay} className={`${play ? "onPlay" : "offPlay"}`}>Play</h2>
-                <Stack spacing={2} direction="row" sx={{ mb: 1, width: 200, color: 'white' }} alignItems="center">
+                <Stack spacing={2} direction="row" sx={{ mb: 1, color: 'white' }} alignItems="center" className="volume-slider">
                     <VolumeDown />
                     <Slider aria-label="Volume" className={play ? "onPlay" : "offPlay"} value={volume} onChange={handleChangeVolume} />
                     <VolumeUp />
                 </Stack>
-                <NavLink to='profile' onClick={()=>{setActive(true)}} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}><h2>About Me</h2></NavLink>
-                <NavLink to='projects' onClick={()=>{setActive(true)}} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}><h2>Projects</h2></NavLink>
-                <NavLink to='/' onClick={()=>{setActive(false)}} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}><h2>Home</h2></NavLink>
+                <NavLink to='profile' onClick={() => { setActive(true) }} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}>
+                    <h2>About Me</h2>
+                </NavLink>
+                <NavLink to='projects' onClick={() => { setActive(true) }} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}><h2>Projects</h2></NavLink>
+                <NavLink to='/' onClick={() => { setActive(false) }} className={(NavStatus) => NavStatus.isActive ? 'active' : ''}><h2>Home</h2></NavLink>
             </div>
-            <div className={`page-container ${active? "active":""}`}>
+            <div className={`page-container ${active ? "active" : ""}`}>
                 <Outlet></Outlet>
             </div>
         </div>
--- portfolio/src/react-app-env.d.ts
+++ portfolio/src/react-app-env.d.ts
@@ -1,2 +1,3 @@
 declare module '*.mp4';
 declare module '*.mp3';
+declare module '*.JPG';

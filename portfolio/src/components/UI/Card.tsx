83e23fcd7b9a012b3b8924c1ff01f8df01112d28
Wenze0614 <wenze0614@gmail.com>
2022-01-13 16:36:46 +0800

    add styling and contents to profile

---
+++ portfolio/src/components/UI/Card.tsx
@@ -0,0 +1,9 @@
+import React,{ReactNode} from 'react'
+import './Card.css'
+export default function Card(props:{children:ReactNode,className?:string}) {
+    return (
+        <div className={`Card ${props.className ? props.className:""}`}>
+            {props.children}
+        </div>
+    )
+}
--- portfolio/src/pages/Profile.tsx
+++ portfolio/src/pages/Profile.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './Profile.css'
+import profile from '../assets/profile.JPG'
+import Card from '../components/UI/Card'
 export default function Profile() {
     return (
-        <div>
-            Hello World!
+        <div className='profile-container'>
+            <Card className='profile-photo-container'>
+                    <img src={profile} alt="myPic"></img>
+            </Card>
+            <Card className='contact-info'>
+                <h3>My Name:<span>Wenze Zhang</span></h3>
+            </Card>
         </div>
     )
 }
